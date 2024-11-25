113cbbc34c3db2e3dd4afad0aceda676d4a2bf0d
Kacper Dygoń <57324463+kacperdygon@users.noreply.github.com>
2024-11-25 11:17:09 +0000

    added fetch to signup and login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,32 +1,65 @@
-
-import "../auth.css";
+"use client";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const usernameInput = useRef<HTMLInputElement>(null);
+  const passwordInput = useRef<HTMLInputElement>(null);
+  const showPasswordCheck = useRef<HTMLInputElement>(null);
+  const [showPassword, setShowPassword] = useState(false);
 
-    return (
-        <div>
-            <form>
-                    <h1>Zaloguj się do Kosmetyczki</h1>
+  return (
+    <div>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const res = await fetch("/api/login", {
+            method: "POST",
+            body: JSON.stringify({
+              username: usernameInput.current?.value,
+              password: passwordInput.current?.value,
+            }),
+          });
+          if (res.status == 401) alert("złe hasło lub nazwa użytkownika");
+          if (res.status == 400) alert("podaj hasło i nazwę użytkownika");
+          if (res.status == 201) alert("zalogowano");
+        }}
+      >
+        <h1>Zaloguj się do Kosmetyczki</h1>
 
-                    <label>
-
-                        <input className="login-input" type="text" placeholder="e-mail/nazwa"/>
-                    </label>
-                    <label>
-                        <input className="login-input" type="password" placeholder="hasło"/>
-
-                    </label>
-                <label>
-                    <input
-                        type="checkbox"
-
-                    />&nbsp;Pokaż hasło
-                </label>
-                    <p>Nie masz jeszcze konta? <a href="register">Zarejestruj się</a></p>
-                    <p>Zapomniałeś hasła? <a href="password-reset">Zresetuj hasło</a></p>
-                <input type="submit" value="Zaloguj się" className="submit-button"/>
-            </form>
-        </div>
-
-    );
+        <label>
+          <input
+            className="login-input"
+            type="text"
+            placeholder="e-mail/nazwa"
+            ref={usernameInput}
+          />
+        </label>
+        <label>
+          <input
+            className="login-input"
+            type={showPassword ? "text" : "password"}
+            placeholder="hasło"
+            ref={passwordInput}
+          />
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            ref={showPasswordCheck}
+            onChange={() => {
+              setShowPassword(!!showPasswordCheck.current?.checked);
+            }}
+          />
+          &nbsp;Pokaż hasło
+        </label>
+        <p>
+          Nie masz jeszcze konta? <a href="register">Zarejestruj się</a>
+        </p>
+        <p>
+          Zapomniałeś hasła? <a href="password-reset">Zresetuj hasło</a>
+        </p>
+        <input type="submit" value="Zaloguj się" className="submit-button" />
+      </form>
+    </div>
+  );
 }
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,26 +1,70 @@
-
+"use client";
+import { useRef, useState } from "react";
 
 export default function Register() {
+  const usernameInput = useRef<HTMLInputElement>(null);
+  const passwordInput = useRef<HTMLInputElement>(null);
+  const secondPasswordInput = useRef<HTMLInputElement>(null);
+  const emailInput = useRef<HTMLInputElement>(null);
 
-    return (
-        <div>
-            <form>
-                <h1>Zarejestruj się w Kosmetyczce</h1>
+  return (
+    <div>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
 
-                <label>
+          if (
+            passwordInput.current?.value == passwordInput.current?.value &&
+            passwordInput.current != null
+          ) {
+            alert("Hasła nie są takie same");
+          }
 
-                    <input className="login-input" type="text" placeholder="e-mail/nazwa"/>
-                </label>
-                <label>
-                    <input className="login-input" type="password" placeholder="hasło"/>
-                </label>
-                <label>
-                    <input className="login-input" type="password" placeholder="powtórz hasło"/>
-                </label>
-                <p>Masz już konto? <a href="login">Zaloguj się</a></p>
-                <input type="submit" value="Zarejestruj się" className="submit-button"/>
-            </form>
-        </div>
+          const res = await fetch("/api/signup", {
+            method: "POST",
+            body: JSON.stringify({
+              username: usernameInput.current?.value,
+              password: passwordInput.current?.value,
+              email: emailInput.current?.value,
+            }),
+          });
+          if (res.status == 409) alert("użytkownik już istnieje");
+          if (res.status == 400)
+            alert("hasło nie spełnia wymagań dotyczących bezpieczeństwa");
+          if (res.status == 201) alert("stworzono użytkownika");
+        }}
+      >
+        <h1>Zarejestruj się w Kosmetyczce</h1>
 
-    );
+        <label>
+          <input
+            className="login-input"
+            type="text"
+            placeholder="e-mail/nazwa"
+            ref={usernameInput}
+          />
+        </label>
+        <label>
+          <input className="login-input" type="password" placeholder="hasło" />
+        </label>
+        <label>
+          <input
+            className="login-input"
+            type="password"
+            placeholder="powtórz hasło"
+            ref={passwordInput}
+          />
+        </label>
+        <p>
+          Masz już konto? <a href="login">Zaloguj się</a>
+        </p>
+        <input
+          type="submit"
+          value="Zarejestruj się"
+          className="submit-button"
+          ref={secondPasswordInput}
+        />
+      </form>
+    </div>
+  );
 }
