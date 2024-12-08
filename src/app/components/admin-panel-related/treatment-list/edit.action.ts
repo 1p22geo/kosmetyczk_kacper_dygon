f78c288b4e5e3ff1956f172a7b00eb8a1defa3ea
1p22geo <1p22geodecki@gmail.com>
2024-12-08 13:34:18 +0100

    feat: edytowanie czegośtam

---
+++ src/app/components/admin-panel-related/treatment-list/edit.action.ts
@@ -0,0 +1,51 @@
+"use server"
+
+import { env } from "process";
+import { MongoClient, ObjectId, WithoutId } from "mongodb";
+import { Treatment } from "@/app/components/admin-panel-related/treatment-list/treatment-list";
+import { redirect } from "next/navigation";
+
+export const edytujZabieg = async (
+  id: ObjectId,
+  nazwa: string,
+  opis: string,
+  cena: number
+) => {
+  "use server";
+  const uri = env.MONGODB_URI
+    ? env.MONGODB_URI
+    : (() => {
+      throw Error("no mongodb URI, set MONGODB_URI environment variable");
+    })();
+
+  const client = new MongoClient(uri);
+  await client.connect();
+
+  const db = client.db("kosmetyczk");
+  const treatments = db.collection<WithoutId<Treatment>>("treatments");
+
+  await treatments.updateOne({
+    _id: id
+  }, {
+    $set: {
+      price: cena,
+      description: opis,
+      title: nazwa
+    }
+  }
+  );
+  await client.close();
+
+
+};
+
+export const EdytujZabiegAction = async (d: FormData) => {
+  "use server";
+  await edytujZabieg(
+    new ObjectId(d.get("id_zabieg")?.toString() ?? ""),
+    d.get("nazwa")?.toString() ?? "",
+    d.get("opis")?.toString() ?? "",
+    parseFloat(d.get("cena")?.toString() ?? ""),
+  );
+  redirect(`/admin-panel?submitted=true`); // to se możesz zmienić
+}
